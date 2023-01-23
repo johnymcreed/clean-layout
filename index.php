ee4f243ad6a2c8b310b591ecaba3a18b53d2ed67
Johny <78428114+johnymcreed@users.noreply.github.com>
2023-01-23 11:09:20 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,93 @@
+<html guest guest-style style="background: black; color: white; font-family: Arial, sans-serif; padding: 0px; margin: 0px; overflow: hidden">
+<head>
+    <title>defbelua · home</title>
+    <link rel="icon" href="./nobg.png">
+    
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="utf-8" http-equiv="encoding">
+    
+    <meta property="og:title" content="defbelua · home" />
+    <meta property="og:type" content="Website" />
+    <meta property="og:url" content="defbelua.000webhostapp.com" />
+    <meta property="og:description" content="Who, could, tell?">
+    <meta property="og:site_name" content="belua">
+</head>
+<div aria-hidden="static-bg" style="z-index: -1; position: absolute; left: 0; right: 0; top: 0; bottom: 0; opacity: .10; background-image: url('./static.gif')"></div>
+<div aria-hidden="overlay-darkener" style="z-index: -2: position: absolute; right: 0; bottom: 0; opacity: .5; background-repeat: no-repeat;">
+<div>
+    <div style="height: 40vh"></div>
+    <div id="s" style="z-index: 20" active>
+        <center>I once thought ...</center>
+        <center style="padding-top: 10px;">
+            <div style="width: 30rem; letter-spacing: 5px; cursor: default">
+                If we care about eachother, why do we follow greed, accept less freedom everyday, 
+                allow political governments to ruin our lives, enrich in poverty. allow over half a dozen americans to be poor, allow violence, death, agony. We can't advance. we just. can't.
+            </div>
+        </center>
+    </div>
+    <!--<div style="height: 10vh"></div>-->
+    <div id="c" unactive>
+        <center style="letter-spacing: 20px;">
+        </center>
+    </div>
+    <!--<div style="height: 10vh"></div>-->
+    <div id="f" unactive>
+    </div>
+    <div style="position: absolute; right: 3px; bottom: 3px; color: #080808; user-select: none">
+        Property of beluaAR&trade;
+    </div>
+</div>
+<style>
+::selection {
+    background: red;
+    color: black;
+}
+a {
+    color: #941d19;
+    text-decoration: none;
+}
+a:hover {
+    transition: .5s;
+    font-size: 17px;
+    color: #751714;
+}
+#f {
+    z-index: 99999;
+    display: inline;
+}
+#c {
+    z-index: 5;
+    display: inline;
+}
+#s {
+    animation: static 10s linear infinite;
+    animation-direction: alternate;
+}
+@keyframes static {
+    0% {
+    }
+    50% {
+        margin-top: -1rem;
+    }
+    80% {
+        margin-bottom: 2rem;
+    }
+    100% {
+        margin-top: 1rem;
+    }
+}
+a:visited {
+    color: #751714;
+}
+</style>
+
+
+<script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
+<script src="./tilt.jquery.js"></script>
+
+<script>
+    $('#s').tilt({
+        scale: 1.2
+    })
+</script>
